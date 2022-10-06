a6a37fbbd59616f9c9013c103e7e2cc80608782f
Michael Dowse <dowsem@amazon.com>
2022-10-06 13:25:49 +0000

    chore: Small refactor to separate preset transforms

--- jest-preset.js
+++ jest-preset.js
@@ -1,17 +1,15 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-const esModules = [
-  '@cloudscape-design',
+const esModules = ['d3-.*', 'internmap'].join('|');
 
-  // `d3-color` and `d3-scale` packages (plus their transitive dependencies) are using the ESM format.
-  // We need to transform them to have them working in tests.
-  'd3-.*',
-  'internmap',
-].join('|');
 module.exports = {
   transform: {
-    [`node_modules/(${esModules})/.+\\.js$`]: require.resolve('./js-transformer'),
+    'node_modules/@cloudscape-design/.+\\.js$': require.resolve('./js-transformer'),
     'node_modules/@cloudscape-design/.+\\.css': require.resolve('./css-transformer'),
+
+    // `d3-color` and `d3-scale` packages (plus their transitive dependencies) are using the ESM format.
+    // We need to transform them to have them working in tests.
+    [`node_modules/(${esModules})/.+\\.js$`]: require.resolve('./js-transformer'),
   },
-  transformIgnorePatterns: [`/node_modules/(?!(${esModules})/).+\\.js$`],
+  transformIgnorePatterns: [`/node_modules/(?!(${esModules}|@cloudscape-design/)).+\\.js$`],
 };
