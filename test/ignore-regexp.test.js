a29295a2a82a1c575bb31f54c2e071bb33386f26
Timo <gasda@amazon.com>
2023-10-24 09:35:50 +0200

    chore: Remove tap and switch to node test runner

--- test/ignore-regexp.test.js
+++ test/ignore-regexp.test.js
@@ -1,6 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-const tap = require('tap');
+const test = require('node:test');
+const assert = require('node:assert');
 const preset = require('../jest-preset');
 
 const ignorePatterns = preset.transformIgnorePatterns.map(pattern => new RegExp(pattern));
@@ -9,15 +10,17 @@ function isPathIgnored(path) {
   return ignorePatterns.some(pattern => pattern.test(path));
 }
 
-tap.equal(isPathIgnored('/node_modules/react/react.js'), true);
-tap.equal(isPathIgnored('/src/component/index.js'), false);
-tap.equal(isPathIgnored('/node_modules/@cloudscape-design/components/index.js'), false);
-tap.equal(isPathIgnored('/node_modules/@cloudscape-design/design-tokens/index.js'), false);
-tap.equal(isPathIgnored('/node_modules/internmap/index.js'), false);
-tap.equal(isPathIgnored('/node_modules/d3-color/index.js'), false);
-tap.equal(isPathIgnored('/node_modules/d3-scale/index.js'), false);
-tap.equal(isPathIgnored('/node_modules/d3-array/index.js'), false);
-tap.equal(isPathIgnored('/node_modules/d3-interpolate/index.js'), false);
-tap.equal(isPathIgnored('/node_modules/d3-format/index.js'), false);
-tap.equal(isPathIgnored('/node_modules/d3-time/index.js'), false);
-tap.equal(isPathIgnored('/node_modules/d3-time-format/index.js'), false);
+test('ignore patterns', () => {
+  assert.strictEqual(isPathIgnored('/node_modules/react/react.js'), true);
+  assert.strictEqual(isPathIgnored('/src/component/index.js'), false);
+  assert.strictEqual(isPathIgnored('/node_modules/@cloudscape-design/components/index.js'), false);
+  assert.strictEqual(isPathIgnored('/node_modules/@cloudscape-design/design-tokens/index.js'), false);
+  assert.strictEqual(isPathIgnored('/node_modules/internmap/index.js'), false);
+  assert.strictEqual(isPathIgnored('/node_modules/d3-color/index.js'), false);
+  assert.strictEqual(isPathIgnored('/node_modules/d3-scale/index.js'), false);
+  assert.strictEqual(isPathIgnored('/node_modules/d3-array/index.js'), false);
+  assert.strictEqual(isPathIgnored('/node_modules/d3-interpolate/index.js'), false);
+  assert.strictEqual(isPathIgnored('/node_modules/d3-format/index.js'), false);
+  assert.strictEqual(isPathIgnored('/node_modules/d3-time/index.js'), false);
+  assert.strictEqual(isPathIgnored('/node_modules/d3-time-format/index.js'), false);
+});
